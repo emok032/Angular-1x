f1f8ef58ae0c8b2c4b7510872684d3c12c29bf31
Eric Mok <emok032@gmail.com>
2016-12-08 00:28:00 -0500

    save

--- userauth/api/controllers/UserController.js
+++ userauth/api/controllers/UserController.js
@@ -6,6 +6,53 @@
  */
 
 module.exports = {
-	
+	// Sign-up for User
+	signup: function(req, res){
+		console.log('Backend Signup');
+
+		var Passwords = require('machinepack-passwords');
+
+		// Encrypt Password
+		Passwords.encryptPassword({
+			password: req.param('password'),
+			difficulty: 10
+		}).exec({
+			error: function(err){
+				return res.negotiate(err);
+			},
+			success: function(encryptedPassword){
+				require('machinepack-gravatar').getImageUrl({
+					emailAddress: req.param('email')
+				}).exec({
+					error: function(){
+						return res.negotiate(err);
+					},
+					success: function(gravatarUrl){
+						// Creating User
+						User.create({
+							name: req.param('name'),
+							email: req.param('email'),
+							password: encryptedPassword,
+							lastLoggedIn: new Date(),
+							gravatarUrl: gravatarUrl
+						}, function userCreated(err, newUser){
+							if(err) {
+								console.log('Error: ' + err);
+								return res.negotiate(err);
+							}
+
+							// Session Variable
+
+							console.log('User Added');
+							
+							return res.json({
+								id: newUser.id
+							});
+						})
+					}
+				})
+			}
+		})
+	}
 };
 
